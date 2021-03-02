7878e90703606c5dcd375213f98d3b2374d9d736
Anna Kombarova <charmomilla@mail.ru>
2021-03-02 11:53:25 +0200

    Activity 3 is resolved, Anna Kombarova

--- SEF - Participants Workspace/SEF/src/sef/module3/activity/FindLargest.java
+++ SEF - Participants Workspace/SEF/src/sef/module3/activity/FindLargest.java
@@ -27,23 +27,18 @@ public class FindLargest {
 		// Find Largest of three numbers
 				
 		int i = 1, j = 2, k = 10;
-		
-		if (i > j) {
-			if // complete code
-			{
-				System.out.println(i + " is Largest of three numbers");
-			} else {
-				System.out.println(k+ " is Largest of three numbers");
-			}
+
+		if(i >= j) {
+			if(i >= k)
+				System.out.println(i + " is the largest number.");
+			else
+				System.out.println(k + " is the largest number.");
 		} else {
-			if // complete code
-			{
-				System.out.println(j+ " is Largest of three numbers");
-			} else {
-				System.out.println(k+ " is Largest of three numbers");
-			}
+			if(j >= k)
+				System.out.println(j + " is the largest number.");
+			else
+				System.out.println(k + " is the largest number.");
 		}
-		
 
 
 	}
